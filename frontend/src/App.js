a475d292a90a852540cff4f081d86e647678cc6f
Donusfirst <richard.v.v2000@gmail.com>
2024-05-24 13:07:37 -0400

    mejoras visuales, Ticket completo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
-import Ticket from './views/Ticket';
+import Ticket from './components/Ticket';
 import Register from './views/Register';
 import PrivateRoute from './components/Auth';
 import PruebasNav from './views/PruebasNav';
+import MesaAyuda from './views/MesaAyuda';
 
 const App = () => {
   return (
@@ -16,7 +17,7 @@ const App = () => {
           <Route path="/ticket" element={<Ticket/>}/>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<PrivateRoute component={Register} />} />
-          <Route path="/PruebasNav" element={<PruebasNav/>} />
+          <Route path="/MesaAyuda" element={<MesaAyuda/>} />
         </Routes>
       </div>
     </Router>
