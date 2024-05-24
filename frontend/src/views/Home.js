8e7e184bb25915d17c788b057bd68880d0bd960d
Donusfirst <richard.v.v2000@gmail.com>
2024-05-24 11:01:35 -0400

    natvar y ticket juntos

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../Styles/Home.css';
 
 function Home() {
 
@@ -12,12 +13,31 @@ function Home() {
 
 
   return (
-    <div className='form-container'>
-      <h2>Home Page</h2>
-      <p>Bienvenido a la página de inicio</p>
-      {/* Agregar el botón para cerrar sesión */}
-      <button onClick={logout}>Cerrar sesión</button>
-    </div>
+    <body>
+    <nav>
+      <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>  
+          <div class="logo">
+            <h1>Navbar</h1>
+          </div>
+          <div class="menu-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">blogs</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">contact</a></li>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </body>
+  
   );
 }
 
--- frontend/src/views/Login.js
+++ frontend/src/views/Login.js
@@ -4,19 +4,33 @@ import '../Styles/FormLogin.css'; // Importa tu archivo de estilos CSS
 import '../Styles/boton.css';
 
 function Login() {
-  const [captchaValido, cambiarCaptchaValido] = useState(null);
   const [usuarioValido, cambiarUsuarioValido] = useState(null);
-  const catpcha = useRef(null);
-   
+  const [formularioEnviado, cambiarFormularioEnviado] = useState(false); // Nuevo estado
+  const [mostrarMensaje, cambiarMostrarMensaje] = useState(false); // Estado para controlar el mensaje de aviso
+  const [mostrarMensajeUsuario, cambiarMostrarMensajeUsuario] = useState(false); // Estado para controlar el mensaje de usuario no válido
+
+  const captcha = useRef(null);
+
   const onChange = () => {
-    if (catpcha.current.getValue()){
-      console.log(catpcha.current.getValue())
+    if (captcha.current.getValue()) {
+      cambiarUsuarioValido(null);
     }
   }
   
   const submit = async (e) => {
     e.preventDefault();
+  
+    cambiarFormularioEnviado(true); // Marcar el formulario como enviado
 
+    if (!captcha.current.getValue()) {
+      cambiarUsuarioValido(false);
+      cambiarMostrarMensaje(true);
+      setTimeout(() => {
+        cambiarMostrarMensaje(false);
+      }, 10000); // Ocultar el mensaje después de 5 segundos
+      return;
+    }
+    
     // Extrae el nombre de usuario y contraseña del formulario
     const nombreUsuario = e.target.elements.nombreUsuario.value;
     const contrasena = e.target.elements.contrasena.value;
@@ -30,75 +44,57 @@ function Login() {
         },
         body: JSON.stringify({ nombreUsuario, contrasena }),
       });
-
+  
       const data = await response.json(); 
       if (data.success) {
         console.log('Inicio de sesión exitoso');
         cambiarUsuarioValido(true);
         // Almacenar el token en el almacenamiento local del navegador
         localStorage.setItem('token', data.token);
-
+  
         window.location.href = '/'
-
+  
       } else {
         console.log('Credenciales incorrectas');
         cambiarUsuarioValido(false);
+        cambiarMostrarMensajeUsuario(true);
+        setTimeout(() => {
+          cambiarMostrarMensajeUsuario(false);
+        }, 10000); // Ocultar el mensaje después de 5 segundos
       }
     } catch (error) {
       console.error('Error al enviar solicitud al servidor:', error);
     }
-    
-    if (catpcha.current.getValue()){
-      console.log('El usuario no es un robot');
-      cambiarUsuarioValido(true);
-      cambiarCaptchaValido(true);
-      console.log(catpcha.current.getValue());
-    } else {
-      console.log('acepta el captcha');
-      cambiarUsuarioValido(false);
-      cambiarCaptchaValido(false);
-    }
   }
-
-
-  // Función para cerrar sesión y eliminar el token
-  const register = () => {
-    // Redirigir al usuario a la página de registro de sesión
-    window.location.href = '/register';
-  };
-  
   return (
     <div className="container">
       <div className="form-container">
         <h2>Iniciar Sesion</h2>
         <form onSubmit={submit}>
           <label>
-            <input type="text" placeholder="Nombre de usuario" name= "nombreUsuario" autocomplete="off"/>
+            <input type="text" placeholder="Nombre de usuario" name= "nombreUsuario" autoComplete="off"/>
           </label>
           <label>
-
             <input type="password" placeholder="Contraseña" name="contrasena" />
           </label>
 
           <div> 
-          <ReCAPTCHA
-          ref={catpcha}
-          sitekey="6LevSKspAAAAAEkO1bS85IlnFZQuLV-WT9jmBD6t"
-          onChange={onChange}
-          className="custom-recaptcha" // Clase para aplicar estilos
-          />
+            <ReCAPTCHA
+              ref={captcha}
+              sitekey="6LevSKspAAAAAEkO1bS85IlnFZQuLV-WT9jmBD6t"
+              onChange={onChange}
+              className="custom-recaptcha" // Clase para aplicar estilos
+            />
           </div>
+          
           <div>
-             <h2></h2>
+            <button type="submit" className='btn10' style={{ margin: "0 auto" }}>Iniciar sesión</button>
           </div>
-          <div>
-          <button type="submit" className='btn10' style={{ margin: "0 auto" }}>Iniciar sesión</button>
-          </div>         
+          {mostrarMensaje && <div className="alerta-captcha"> Favor complete el Captcha.</div>}    
+          {mostrarMensajeUsuario && <div className="error-mensaje">Usuario no válido</div>}     
         </form>
-        
       </div>
     </div>
-    
   );
 }
 
